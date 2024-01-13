6cfdf8b6cf33681ff3a4342a03bc5c68d95a01b5
zoro <latkar.mihir@gmail.com>
2024-01-13 12:18:39 +0530

    creating backend files and pushing on github

---
+++ streamer/src/test/java/com/chatflix/streamer/StreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatflix.streamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
